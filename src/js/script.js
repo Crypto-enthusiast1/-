e227ebedc835bcbab01e05da36a0a17101c10e51
Artem <andrei28123@gmail.com>
2024-03-25 23:47:32 +0100

    Slider for PC version is ready

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+   $('.carousel__inner').slick({
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png" alt="arrow"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_rigth.png" alt="arrow"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               arrows: false,
+               dots: true
+            }
+         },
+      ]
+   });
+});
