b3a1d68642d9fe8b54ccd6134534083686c0ff6e
Artem <andrei28123@gmail.com>
2024-04-03 01:24:24 +0200

    Footer is ready with mobile version

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-   $(document).ready(function () {
-      $('.carousel__inner').slick({
-         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png" alt="arrow"></button>',
-         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_rigth.png" alt="arrow"></button>',
-         responsive: [
-            {
-               breakpoint: 992,
-               settings: {
-                  arrows: false,
-                  dots: true,
-               },
+   document.cookie = "name=value; SameSite=None; Secure; path=/";
 
+   $('.carousel__inner').slick({
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png" alt="arrow"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_rigth.png" alt="arrow"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               arrows: false,
+               dots: true,
             },
-         ]
-      });
+
+         },
+      ]
    });
 
    const btnMoreInfo = document.querySelectorAll('.item__link');
