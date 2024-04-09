6ec06e4e5ec2ae7463034c26c86ca439c77f9959
Artem <andrei28123@gmail.com>
2024-04-10 01:18:26 +0200

    The functionality for sending data by email is not fully ready

--- src/js/script.js
+++ src/js/script.js
@@ -254,7 +254,28 @@ document.addEventListener('DOMContentLoaded', function () {
    })
 
 
+   //Send data on server
 
+   $('form').submit(function (e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+         return;
+      }
+
+      $.ajax({
+         type: 'POST',
+         url: 'mailer/smart.php',
+         data: $(this).serialize()
+      }).done(function () {
+         $(this).find('input').val('');
+         $('#consultation, #order').fadeOut();
+         $('.overlay, #thanks').fadeIn('slow');
+         $('form').trigger('reset');
+      });
+      return false
+
+   })
 
 
 });
